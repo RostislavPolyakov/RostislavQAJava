e0576d1da205160fbfe9df5ed2cf6ae3de0c866b
Rostislav <polyakovorion987@gmail.com>
2020-12-03 21:41:03 +0200

    QC

---
+++ src/com/qa/lesson03/Array.java
@@ -0,0 +1,12 @@
+package com.qa.lesson03;
+
+public class Array {
+    public static void main(String[] args) {
+
+        int[][] array = new int[3][2];
+        array[1][0] = 5;
+//        array[]
+//        0
+
+    }
+}
